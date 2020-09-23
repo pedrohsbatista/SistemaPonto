56442383add75e4a302261b12426543a55c10bca
Pedro Batista <henrique_pedro217@hotmail.com>
2020-09-22 21:03:36 -0300

    Launch, formtação horário e cor checkbox

---
+++ start.bat
@@ -0,0 +1,5 @@
+cd C:\Users\Pedro\3D Objects\SistemaPonto\SistemaPonto-Web
+start cmd.exe /k "ng serve --open"
+
+cd C:\Users\Pedro\3D Objects\SistemaPonto\SistemaPonto.Api
+start cmd.exe /k "dotnet watch run"
